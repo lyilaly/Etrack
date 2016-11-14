3707ba57abbad19e6e01682df2fde5c9feb562d7
Lionelo Espina <espina.lionelo@gmail.com>
2016-11-14 14:29:55 -0400

    Role feature added

---
+++ src/Etrack/Features/Administration/Roles/RoleViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Etrack.Features.Administration.Roles
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+
+
+    public class RoleViewModel
+    {
+        [Display(Name = "Role Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+    public class DeleteRoleViewModel
+    {
+        [Display(Name = "Role Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
